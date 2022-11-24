c497b0acdaa21095100a1e7cc8dfe80e46c8403b
101336421 <101336421@georgebrown.ca>
2022-11-24 17:55:16 -0500

    Working Lab Test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import WeatherList from './WeatherList';
+
+function App() {
+	return (
+		<div style={{ justifyContent: 'center', textAlign: 'center', background: 'pink' }}>
+			<h1 style={{ color: 'black' }}>Lab Test 2 - Vaibhav Guliani (101336421)</h1>
+			<WeatherList />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/WeatherList.js
@@ -0,0 +1,104 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BiWind, BiCurrentLocation } from 'react-icons/bi';
+
+export default class WeatherList extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			input: '',
+			main: [ {} ],
+			weathers: [ {} ],
+			name: '',
+			sys: [ {} ],
+			wind: [ {} ]
+		};
+	}
+
+	getWeatherData = (value) => {
+		axios
+			.get(
+				`https://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&APPID=7674b666b838ad00794142c2eb0231af`
+			)
+			.then((res) => {
+				console.log(res.data);
+				this.setState({ weathers: res.data.weather });
+				this.setState({ main: res.data.main });
+				this.setState({ name: res.data.name });
+				this.setState({ sys: res.data.sys });
+				this.setState({ wind: res.data.wind });
+			})
+			.catch((e) => {
+				if (e.response.status === 404) {
+					console.log('City not Found. Try Another!');
+					alert('City not Found. Try Another!');
+				}
+			});
+	};
+	componentDidMount = () => {
+		this.getWeatherData('Toronto');
+	};
+
+	getCity = (e) => {
+		this.setState({ input: e.target.value });
+	};
+
+	getCityData = () => {
+		console.log(this.state.input);
+		this.getWeatherData(this.state.input);
+	};
+
+	render() {
+		return (
+			<div
+				style={{
+					textAlign: 'center',
+					justifyContent: 'center'
+					
+					
+				}}
+			>
+				<div style={{ paddingBottom: '30px', color: 'black'  }}>
+					<h3>Toronto Shown by Default</h3>
+					<input type="text" onChange={this.getCity} style={{ height: '38px', verticalAlign: 'middle' }} />
+					<Button onClick={this.getCityData}>Get Your City Weather</Button>
+				</div>
+				<div>
+					<Card style={{ paddingTop: '10px', background: '#FAF9F6' }}>
+						<div style={{ display: 'flex', textAlign: 'center', justifyContent: 'center' }}>
+							<Card.Img
+								style={{ width: '200px' }}
+								variant="top"
+								src={`http://openweathermap.org/img/wn/${this.state.weathers[0].icon}@2x.png`}
+							/>
+							<Card.Text style={{ fontSize: '80px', lineHeight: '250px' }}>
+								{this.state.main.temp} °C
+							</Card.Text>
+						</div>
+						<Card.Body>
+							<Card.Title style={{ fontSize: '50px' }}>
+								<BiCurrentLocation /> {this.state.name}, {this.state.sys.country}
+							</Card.Title>
+							<Card.Text style={{ fontSize: '40px' }}>
+								Feels like {this.state.main.feels_like} °C. {this.state.weathers[0].main},{' '}
+								{this.state.weathers[0].description}
+							</Card.Text>
+							<Card.Text style={{ fontSize: '25px' }}>
+								Minimum Temperature: {this.state.main.temp_min} °C | Maximum Temperature:{' '}
+								{this.state.main.temp_max} °C
+							</Card.Text>
+
+							<Card.Text style={{ fontSize: '25px' }}>
+								{' '}
+								<BiWind />Wind Speed: {this.state.wind.speed} m/s
+							</Card.Text>
+						</Card.Body>
+					</Card>
+				</div>
+			</div>
+		);
+	}
+}
